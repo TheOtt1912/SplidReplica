1c2e8a2e038ba17ed720fb002ba715dd293dc3ef
Usman <usmanmukaty@hotmail.com>
2026-03-27 18:59:25 +0400

    First Schema and some auth code

---
+++ splid_app/schema.sql
@@ -0,0 +1,50 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS trips;
+DROP TABLE IF EXISTS usersInTrip;
+
+CREATE TABLE users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP 
+)STRICT;
+
+CREATE TABLE trips(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    trip_name TEXT NOT NULL,
+    creator_id INTEGER NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (creator_id) REFERENCES users(id)
+)STRICT;
+
+CREATE TABLE usersInTrip(
+    user_id INTEGER NOT NULL,
+    trip_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (trip_id) REFERENCES trips(id),
+    PRIMARY KEY (user_id, trip_id)
+)STRICT;
+
+CREATE TABLE transactions(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    amount REAL NOT NULL,
+    title TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    trip_id INTEGER NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (trip_id) REFERENCES trips(id)
+)STRICT;
+
+CREATE TABLE debts(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    transaction_id INTEGER NOT NULL,
+    owed_by_id INTEGER NOT NULL,
+    amount REAL NOT NULL,
+    status TEXT CHECK(status IN ('owing','paid')) NOT NULL,
+    FOREIGN KEY (transaction_id) REFERENCES transactions(id),
+    FOREIGN KEY (owed_by_id) REFERENCES users(id)
+
+)STRICT;
